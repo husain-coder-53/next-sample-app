f4eb3ac6eec09edc5121fca866d4c26f0c1aa62c
Husain Amravatiwala <76838013+husain-coder-53@users.noreply.github.com>
2023-08-12 13:01:50 +0000

    First commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import { ThemeProvider } from '@/components/theme-provider'
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Affiliate App',
+  description: 'All solutions for making your products successful',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>{children}</ThemeProvider></body>
+    </html>
+  )
+}
